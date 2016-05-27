a0961cb2f7928c45fa7d976c3d5ed645b2485b37
Pranav Prakash <pranny@gmail.com>
2016-05-27 19:03:17 +0530

    Adding User Persona

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,23 +2,32 @@ require 'octocore'
 
 class UserController < ApplicationController
 
-  get '/:userid?' do
+  # specific to user
+  get '/' do
     @enterprise = Octo::Enterprise.first
-    user = Octo::User.find_by_enterprise_id_and_id(
-      @enterprise.id,
-      @params[:userid].to_i
-    )
-    if user
-      @success = true
-      @user = user
-      @res = Octo::UserTimeline.fakedata(@user).collect do |x|
-        x.human_readable
+    @success = false
+
+    if @params.has_key?('userid')
+      user = Octo::User.find_by_enterprise_id_and_id(
+        @enterprise.id,
+        @params['userid'].to_i
+      )
+      if user
+        @success = true
+        @user = user
+        @res = Octo::UserTimeline.fakedata(@user).collect do |x|
+          x.human_readable
+        end
+        start_ts = @params.fetch('start_time', 7.days.ago)
+        end_ts = @params.fetch('end_time', Time.now)
+        @persona = Octo::UserPersona.fakedata(@user, start_ts..end_ts)
+      else
+        @success = false
       end
-    else
-      @success = false
     end
     erb :user
   end
+
 
 end
 
