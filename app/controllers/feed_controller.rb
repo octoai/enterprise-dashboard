053e296509ffd9690e9265dd3a7a5a168e7956be
Pranav Prakash <pranny@gmail.com>
2016-05-30 19:22:01 +0530

    Ref T37. Fixes T37. Updating dashboard to use new models

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -7,30 +7,50 @@ class FeedController < ApplicationController
   get '/graphdata' do
     start_time = Time.parse(params[:startTime])
     end_time = Time.parse(params[:endTime])
+    puts start_time
+    puts end_time
     graph_type = params[:graph_type]
 
     enterprise = get_enterprise
-    json_arr = []
-    if graph_type == 'ctr'
-      (start_time.to_i..end_time.to_i).step(86400) do |x|
+    data = []
+    args = {
+      enterprise_id: enterprise.id,
+      ts: start_time..end_time
+    }
+    case graph_type
+    when 'ctr'
+      (start_time.to_i..end_time.to_i).step(1.day) do |x|
         Data = Octo::Ctr.data( enterprise[:id], x)
-        jsonData = { 
+        jsonData = {
           ts: Data[:ts],
           value: Data[:value]
         }
-        json_arr.push(jsonData)
+        data.push(jsonData)
       end
-    else
-      (start_time.to_i..end_time.to_i).step(86400) do |x|
-        ConversionData = Octo::Conversions.data( enterprise[:id], Octo::Conversions::NEWSFEED, x)
-        jsonData = { 
-          ts: ConversionData[:ts],
-          value: ConversionData[:value]
-        }
-        json_arr.push(jsonData)
-      end
+    when 'pageload'
+      _args = args.merge({
+        pageload_type: Octo::PageloadTime::NEWSFEED,
+        counter_type: Octo::Counter::TYPE_DAY
+      })
+      res = Octo::PageloadTime.fakedata(_args)
+      data = res.collect { |x| { ts: x.ts, value: x.time } }
+    when 'engagement'
+      _args = args.merge({
+        engagement_type: Octo::EngagementTime::NEWSFEED,
+        counter_type: Octo::Counter::TYPE_DAY,
+      })
+      res = Octo::EngagementTime.fakedata(_args)
+      data = res.collect { |x| { ts: x.ts, value: x.time } }
+    when 'absolute'
+      _args = args.merge({
+        type: Octo::Counter::TYPE_DAY
+      })
+      puts _args
+      res = Octo::NewsfeedHit.fakedata(_args)
+      data = res.collect { |x| { ts: x.ts, value: x.count }}
     end
-    {data: json_arr}.to_json
+    { data: data }.to_json
   end
 
 end
+
